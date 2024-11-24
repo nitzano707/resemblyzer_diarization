f192a386835554d2dfe2cfc2e4e0cdc5fd1fb75a
nitzano707 <nitzan.orot@gmail.com>
2024-11-24 11:36:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -56,4 +56,5 @@ def encode_audio():
 asgi_app = WsgiToAsgi(app)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # קבלת פורט מ-Render
+    app.run(host="0.0.0.0", port=port)
